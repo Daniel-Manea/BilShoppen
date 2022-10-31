0705af5b15a6fce0845b010fca0221888993814f
Aronel Daniel Manea <danimane4info@gmail.com>
2022-10-31 08:20:42 +0100

    Initial commit

---
+++ BilShoppen/Database/DB.cs
@@ -0,0 +1,37 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DocumentReference = Google.Cloud.Firestore.DocumentReference;
+
+namespace BilShoppen.Database
+{
+    internal class DB
+    {
+
+        public static void InitializeDB()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"car-dealership-366710-firebase-adminsdk-66tky-817218f94a.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+        }
+
+        private static readonly string project = "car-dealership-366710";
+
+        private static readonly FirestoreDb db = FirestoreDb.Create(project);
+
+        static public async void AddUser(object userInfo)
+        {
+            DocumentReference docRef = db.Collection("users").Document("test");
+            Dictionary<string, object> user = new()
+            {
+                {
+                    "Name", userInfo
+                },
+            };
+            await docRef.SetAsync(user);
+        }
+    }
+}
---
+++ BilShoppen/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using BilShoppen.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace BilShoppen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DB.InitializeDB();
+            InitializeComponent();
+
+        }
+
+       public class User
+        {
+            private string firstname = "unknown";
+
+            private string lastname = "unknown";
+
+            private object Name = "unknown";
+
+
+            protected void SetName()
+            {
+               Name = new { FirstName = "Daniel", LastName = "Manea" };
+            }
+
+            public static object GetName()
+            {
+
+                return new { FirstName = "Daniel", LastName = "Manea" }; ;
+            }
+        }
+
+        private void Button_Click_Ny(object sender, RoutedEventArgs e)
+        {
+    
+            DB.AddUser(User.GetName());
+        }
+        private void Button_Click_Åben(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Email(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Gem(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Afslut(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
